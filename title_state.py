dfa7de6f893ed248c80289f028dbe4bffe1a1e4e
HyeonSuLee20000209 <gustn1164@naver.com>
2022-11-29 01:06:33 +0900

    타이틀 화면 구현 완료

--- title_state.py
+++ title_state.py
@@ -6,19 +6,29 @@ import play_state
 
 image = True
 
+start = None
+start_color = 0
+
+finish = None
+finish_color = 0
+
 
 def enter():
-    global image
+    global image, start, finish
     image = load_image('resource/Title.png')
+    start = load_font('resource/ENCR10B.TTF', 50)
+    finish = load_font('resource/ENCR10B.TTF', 50)
 
 
 def exit():
-    global image
-    del image
+    global image, start, finish
+    del image, start, finish
 
-count = 0
+
 def handle_events():
-    global image, count
+    global image
+    global start, start_color
+    global finish, finish_color
 
     events = get_events()
     for event in events:
@@ -32,23 +42,32 @@ def handle_events():
                     game_framework.quit()
                 case pico2d.SDLK_SPACE:
                     game_framework.change_state(play_state)
-        elif event.type == SDL_MOUSEBUTTONDOWN:
-            # 게임 시작
-            if event.button == 1:
-                if 0 < event.x < 110 * 1000 // image.w:
-                    pass
-                    # if 52 * 600 // image.h < event.y < 82 * 600 // image.h:
-                    #     print('d')
-                    #     game_framework.change_state(play_state)
-                    # elif 96 * 600 // image.h < event.y < 126 * 600 // image.h:
-                    #     game_framework.change_state(play_state)
-
+        elif event.type == SDL_MOUSEMOTION:
+            if 0 < event.x < 109 * 1000 // image.w:
+                if 67 * 600 // image.h < event.y < 99 * 600 // image.h:
+                    start_color = 125
+                elif 112 * 600 // image.h < event.y < 142 * 600 // image.h:
+                    finish_color = 125
+                else:
+                    start_color, finish_color = 0, 0
+            else:
+                start_color, finish_color = 0, 0
+        if event.type == SDL_MOUSEBUTTONDOWN:
+            if 0 < event.x < 109 * 1000 // image.w:
+                if 67 * 600 // image.h < event.y < 99 * 600 // image.h:
+                    if event.button == 1:
+                        game_framework.change_state(play_state)
+                elif 112 * 600 // image.h < event.y < 142 * 600 // image.h:
+                    if event.button == 1:
+                        game_framework.quit()
 
 
 def draw():
-    global image
+    global image, start, finish
     clear_canvas()
     image.draw(500, 300, 1000, 600)
+    start.draw(10, 112 * 600 // image.h, 'Game Start', (start_color, start_color, start_color))
+    finish.draw(75, 67 * 600 // image.h, 'Close', (finish_color, finish_color, finish_color))
     update_canvas()
 
 
